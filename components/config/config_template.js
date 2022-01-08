b2d25982fb2a8b4cfcd50716ff541985c5adc861
Michael Uche <michaelphardy00@gmail.com>
2022-01-08 17:19:31 +0100

    updated

---
+++ components/config/config_template.js
@@ -0,0 +1,16 @@
+//make sure you rename to 'config''
+export default{
+  APP_TITLE: 'PCN APAPA',
+  APP_DESCRIPTION: 'Welcome to the Presbyterian church of Nigeria Apapa Parish official app.',
+  BANK_ACCOUNT_NAME:'',
+  BANK_ACCOUNT_NUMBER:'',
+  CHURCH_NAME:'Presbyterian',
+  CHURCHES_AROUND:'Presbyterian',
+  PLACE_TYPE:'Church',
+  SEARCH_RADIUS:10000,
+  PLACES_API:'',
+  PLACE_ID:'ChIJUXxCoXCJOxAR-x_iHCLE4hw',
+  CHURCH_LNG:3.3271676,
+  CHURCH_LAT:6.4491508
+  
+}
